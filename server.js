420d01bae990779cabb872fd26394f38cfb5c0cc
Lucas Lima <lucas.developmaster@gmail.com>
2020-03-27 22:38:56 -0300

    Primeiro commit

---
+++ server.js
@@ -0,0 +1,33 @@
+// Importacao de pacotes
+const express = require('express');
+const path = require('path');
+
+// Configuracao do socket
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+// Configuracao de pagina estatica
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/', (req, res) => {
+    res.render('index.html');
+});
+
+let messages = [];
+
+io.on('connection', socket => {
+    console.log(`Socket conectado: ${socket.id}`);
+
+    socket.emit('previousMessages', messages);
+
+    socket.on('sendMessage', data => {
+        messages.push(data);
+        socket.broadcast.emit('receivedMessage', data);
+    });
+});
+
+server.listen(3333);
